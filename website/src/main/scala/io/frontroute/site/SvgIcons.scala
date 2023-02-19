62b87a2117b0523366991b5aec4aa04bf5079123
Iurii Malchenko <i@yurique.com>
2023-02-19 23:31:38 +0100

    cruft

--- website/src/main/scala/io/frontroute/site/SvgIcons.scala
+++ website/src/main/scala/io/frontroute/site/SvgIcons.scala
@@ -46,10 +46,6 @@ object SvgIcons {
               """xmlns="http://www.w3.org/2000/svg"""",
               ""
             )
-            .replaceAll(
-              """xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" baseProfile="full"""",
-              ""
-            )
         }
       }
 
