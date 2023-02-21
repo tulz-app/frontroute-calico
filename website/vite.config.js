bd431abff18b53c99170290bf348c771e588ad88
Iurii Malchenko <i@yurique.com>
2023-02-21 21:47:30 +0100

    use rollup-commonjs plugin directly

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,20 +1,13 @@
-import {defineConfig} from 'vite'
 import {resolve} from 'path'
 import {createHtmlPlugin} from 'vite-plugin-html'
-// import commonjs from '@rollup/plugin-commonjs';
-const fs = require('fs');
+import commonjs from '@rollup/plugin-commonjs';
 
 import scalaVersion from './scala-version'
 
 // https://vitejs.dev/config/
 export default ({mode}) => {
   const mainJS = `target/scala-${scalaVersion}/website-${mode === 'production' ? 'opt' : 'fastopt'}/main.js`
-  const moduleJS = `module.js`
-  console.log('mainJS', mainJS)
-  fs.writeFileSync(moduleJS, `import './${mainJS}'`)
-
   const script = `<script type="module" src="${mainJS}"></script>`
-  // const script = `<script type="module" src="/${moduleJS}"></script>`
 
   /** @type {import('vite').UserConfig} */
   return {
@@ -25,30 +18,12 @@ export default ({mode}) => {
     publicDir: './src/main/public',
     build: {
       outDir: 'dist/v/0.17.x-calico',
-      commonjsOptions: {
-        // include: [mainJS],
-        // include: [mainJS, /node-modules/],
-        // include: [mainJS, /highlight.js/, /marked/],
-        // transformMixedEsModules: true,
-        // strictRequires: 'debug',
-        strictRequires: true,
-      }
     },
     optimizeDeps: {
-      disabled: false,
-      force: true,
-      include: [
-        // moduleJS,
-        mainJS,
-        // 'highlight.js',
-        // 'marked',
-        // 'marked.js',
-        // moduleJS,
-      ]
+      disabled: true,
     },
     plugins: [
-      // commonjs({
-      // }),
+      ...(mode === 'production' ? [commonjs()] : []),
       createHtmlPlugin({
         minify: mode === 'production',
         inject: {
